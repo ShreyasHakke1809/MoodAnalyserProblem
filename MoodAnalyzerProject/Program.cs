bfea4f684f5af27aa64c255001c376ce4bbfce87
ShreyasHakke1809 <137746695+ShreyasHakke1809@users.noreply.github.com>
2023-08-05 11:41:50 +0530

    Update Program.cs

--- MoodAnalyzerProject/Program.cs
+++ MoodAnalyzerProject/Program.cs
@@ -7,8 +7,6 @@ namespace MoodAnalyzerProject
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mood Analyzer Program");
-            MoodAnalyze mood = new MoodAnalyze("I am Happy");
-            Console.WriteLine("Mood is "+mood.AnalyzeMood());
         }
     }
 }
